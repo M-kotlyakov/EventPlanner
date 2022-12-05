ac164461ecf32a82fb93932ddf512df277c7c255
Max <m.kotlyakov.02@mail.ru>
2022-12-05 17:49:46 +0300

    correct mistake

--- app/src/main/java/test/mertech/eventplanner/mvvm/presentation/utils/IconsUrls.kt
+++
@@ -1,8 +0,0 @@
-package test.mertech.eventplanner.mvvm.presentation.utils
-
-val iconsUrl = listOf(
-    "https://cdn-icons-png.flaticon.com/512/1163/1163662.png",
-    "https://cdn-icons-png.flaticon.com/512/4503/4503021.png",
-    "https://cdn-icons-png.flaticon.com/512/1146/1146799.png",
-    "https://cdn-icons-png.flaticon.com/512/653/653429.png"
-)
